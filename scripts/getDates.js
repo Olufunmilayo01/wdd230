ea90a01f3ed5376693b2fb4704fb1aeb299366ad
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2025-01-25 00:17:27 +0100

    Week 1 assignment completed

---
+++ scripts/getDates.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+  const currentYear = new Date().getFullYear();
+  // Set the copyright year in the first paragraph of the footer
+  document.getElementById("currentYear").textContent = ` ${currentYear}`;
+
+  // Get the last modified date of the document
+  const lastModified = document.lastModified;
+  // Set the last modified date in the second paragraph of the footer
+  document.getElementById(
+    "lastModified"
+  ).textContent = `Last Modified: ${lastModified}`;
+});
