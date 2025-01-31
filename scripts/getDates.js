09d5e2d4f55c9949d01e4445cb00bc0c111182a7
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2025-01-31 14:18:00 +0100

    Home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  
   const currentYear = new Date().getFullYear();
   // Set the copyright year in the first paragraph of the footer
   document.getElementById("currentYear").textContent = ` ${currentYear}`;
@@ -10,4 +9,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById(
     "lastModified"
   ).textContent = `Last Modified: ${lastModified}`;
+});
+
+const hamburgerElement = document.querySelector("#myButton");
+const navElement = document.querySelector(".menuLinks");
+
+hamburgerElement.addEventListener("click", () => {
+  navElement.classList.toggle("open");
+  hamburgerElement.classList.toggle("open");
+
+  if (navElement.classList.contains("open")) {
+    hamburgerElement.textContent = "❎";
+  } else {
+    hamburgerElement.textContent = "☰";
+  }
+});
+
+const darkModeButton = document.querySelector("#darkModeButton");
+const mainElement = document.querySelector("main");
+
+// Toggle Dark Mode
+darkModeButton.addEventListener("click", () => {
+  mainElement.classList.toggle("dark-mode");
+
+  // Change button text based on mode
+  if (mainElement.classList.contains("dark-mode")) {
+    darkModeButton.textContent = "☀";
+  } else {
+    darkModeButton.textContent = "🌙";
+  }
 });
