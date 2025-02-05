2e8c2ccfcb0c3d5bd534ab3988347a1ffb0bdb75
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2025-02-05 18:32:48 +0100

    Adjustments made

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -39,3 +39,10 @@ darkModeButton.addEventListener("click", () => {
     darkModeButton.textContent = "🌙";
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let count = localStorage.getItem("visitCount") || 0;
+  count++;
+  localStorage.setItem("visitCount", count);
+  document.getElementById("visit-count").textContent = count;
+});
